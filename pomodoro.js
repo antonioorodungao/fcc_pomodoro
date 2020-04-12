4eb6e1da4dac76b363893d2383aa7d81b2f6dc9a
Antonio Oro Dungao <antonioorodungao@yahoo.com>
2020-04-12 20:51:13 +0300

    To fix later

--- pomodoro.js
+++ pomodoro.js
@@ -9,14 +9,15 @@ let el_timerLabel = document.querySelector("#timer-label");
 let audio_beep = document.querySelector("#beep");
 
 let el_sessionlength = document.querySelector("#session-length");
-let sl_value = parseInt(el_sessionlength.value);
 let el_breaklength = document.querySelector("#break-length");
 let bl_value = parseInt(el_breaklength.value);
+let sl_value = parseInt(el_sessionlength.value);
 
 let current_sl_seconds = sl_value * 60;
 let current_bl_seconds = bl_value * 60;
 let status = "Session";
 let isPaused = true;
+let intervalId;
 
 (function () {
   handleReset();
@@ -29,47 +30,48 @@ let isPaused = true;
   el_reset.addEventListener("click", handleReset);
 })();
 
-setInterval(() => {
-  el_timerLabel.textContent = status;
-
-  if (!isPaused) {
-    if (status === "Session") {
-      current_sl_seconds = current_sl_seconds - 1;
-      //   el_display.value =
-      //     Math.floor(current_sl_seconds / 60) + ":" + (current_sl_seconds % 60);
-      updateDisplay();
-    } else {
-      current_bl_seconds = current_bl_seconds - 1;
-      //   el_display.value =
-      //     Math.floor(current_sl_seconds / 60) + ":" + (current_sl_seconds % 60);
-      updateDisplay();
-    }
-    if (current_sl_seconds === 0 && status === "Session") {
-      status = "Break";
-      current_sl_seconds = sl_value * 60;
-      audio_beep.play();
-    } else if (status === "Break" && current_bl_seconds === 0) {
-      status = "Session";
-      current_bl_seconds = bl_value * 60;
-      audio_beep.play();
-    }
-  }
-}, 1000);
-
 function handlePauseStart(e) {
   e.preventDefault();
+
   if (isPaused) {
     isPaused = false;
+    intervalId = setInterval(() => {
+      el_timerLabel.textContent = status;
+      console.log("Text Content " + el_display.textContent);
+      if (!isPaused) {
+        if (status === "Session") {
+          current_sl_seconds = current_sl_seconds - 1;
+          updateDisplay();
+        } else {
+          current_bl_seconds = current_bl_seconds - 1;
+          updateDisplay();
+        }
+        if (current_sl_seconds === 0 && status === "Session") {
+          status = "Break";
+          current_sl_seconds = sl_value * 60;
+          updateDisplay();
+          audio_beep.play();
+        } else if (status === "Break" && current_bl_seconds === 0) {
+          status = "Session";
+          current_bl_seconds = bl_value * 60;
+          updateDisplay();
+          audio_beep.play();
+        }
+      }
+    }, 1000);
+    console.log("Set:" + intervalId);
   } else {
     isPaused = true;
+    clearInterval(intervalId);
+    console.log("Clear:" + intervalId);
   }
 }
 
 function handleReset(e) {
-  el_sessionlength = document.querySelector("#session-length");
-  el_breaklength = document.querySelector("#break-length");
-  sl_value = parseInt(el_sessionlength.value);
-  bl_value = parseInt(el_breaklength.value);
+  el_sessionlength.textContent = 25;
+  el_breaklength.textContent = 5;
+  sl_value = parseInt(el_sessionlength.textContent);
+  bl_value = parseInt(el_breaklength.textContent);
   current_sl_seconds = sl_value * 60;
   current_bl_seconds = bl_value * 60;
   isPaused = true;
@@ -79,7 +81,7 @@ function handleReset(e) {
 
 function updateDisplay() {
   if (status === "Session") {
-    el_display.value =
+    el_display.textContent =
       Math.floor(current_sl_seconds / 60).toLocaleString("en-US", {
         minimumIntegerDigits: 2,
         useGrouping: false,
@@ -90,7 +92,7 @@ function updateDisplay() {
         useGrouping: false,
       });
   } else if (status === "Break") {
-    el_display.value =
+    el_display.textContent =
       Math.floor(current_bl_seconds / 60).toLocaleString("en-US", {
         minimumIntegerDigits: 2,
         useGrouping: false,
@@ -119,6 +121,6 @@ function handleSettingClickEvent(e) {
       if (sl_value >= 1 && sl_value <= 60) sl_value = sl_value - 1;
       break;
   }
-  el_breaklength.value = bl_value;
-  el_sessionlength.value = sl_value;
+  el_breaklength.textContent = bl_value;
+  el_sessionlength.textContent = sl_value;
 }
